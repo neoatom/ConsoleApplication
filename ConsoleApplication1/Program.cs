5e9f3a559ddf9c3b97d4a82606c81f68549a30fc
junix <neoatom2k17@gmail.com>
2018-06-06 11:38:11 +0800

    initial commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 namespace ConsoleApplication1 {
  class Program {
   static void Main(string[] args) {
+
+   Console.WriteLine("Hello World!");
+   Console.ReadLine();
+
   }
  }
 }
